6418a44be572f139ecb2dce41ff3145295f55dc1
reya <reya@lume.nu>
2023-12-12 15:55:54 +0700

    feat: add ndk cache

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+import Database from "bun:sqlite";
 import NDK, {
-  NDKEvent,
-  NDKFilter,
-  NDKSubscriptionCacheUsage,
-  ProfilePointer,
-} from '@nostr-dev-kit/ndk';
-import { ndkAdapter } from '@nostr-fetch/adapter-ndk';
-import { Elysia, t } from 'elysia';
-import { NostrFetcher, normalizeRelayUrlSet } from 'nostr-fetch';
-import { nip19 } from 'nostr-tools';
+	NDKEvent,
+	NDKFilter,
+	NDKSubscriptionCacheUsage,
+	ProfilePointer,
+} from "@nostr-dev-kit/ndk";
+import { ndkAdapter } from "@nostr-fetch/adapter-ndk";
+import { Elysia, t } from "elysia";
+import { NostrFetcher, normalizeRelayUrlSet } from "nostr-fetch";
+import { nip19 } from "nostr-tools";
+import { NDKCacheAdapterTauri } from "./cache";
 
 let ndk: NDK;
 let fetcher: NostrFetcher;
@@ -15,277 +17,279 @@ let fetcher: NostrFetcher;
 const FETCH_LIMIT = 20;
 
 const app = new Elysia()
-  .onStart(async () => {
-    console.log(Bun.env.CONFIG_DIR);
-    console.log(Bun.env.PUBKEY);
-    console.log(Bun.env.PRIVKEY);
-    console.log(Bun.env.BUNKER);
-    console.log(Bun.env.OUTBOX);
+	.onStart(async () => {
+		console.log(Bun.env.CONFIG_DIR);
+		console.log(Bun.env.PUBKEY);
+		console.log(Bun.env.PRIVKEY);
+		console.log(Bun.env.BUNKER);
+		console.log(Bun.env.OUTBOX);
 
-    /*
-    const db = new Database(`${Bun.env.CONFIG_DIR}/lume_v2.db`, {
-      create: false,
-      readonly: false,
-      readwrite: true,
-    });
-    */
+		const db = new Database(`${Bun.env.CONFIG_DIR}/lume_v2.db`, {
+			create: false,
+			readonly: false,
+			readwrite: true,
+		});
 
-    const explicitRelayUrls = normalizeRelayUrlSet([
-      'wss://relay.damus.io',
-      'wss://relay.nostr.band',
-      'wss://nos.lol',
-      'wss://nostr.mutinywallet.com',
-    ]);
+		const cacheAdapter = new NDKCacheAdapterTauri(db);
 
-    // #TODO: user should config outbox relays
-    const outboxRelayUrls = normalizeRelayUrlSet(['wss://purplepag.es']);
+		const explicitRelayUrls = normalizeRelayUrlSet([
+			"wss://relay.damus.io",
+			"wss://relay.nostr.band",
+			"wss://nos.lol",
+			"wss://nostr.mutinywallet.com",
+		]);
 
-    // #TODO: user should config blacklist relays
-    const blacklistRelayUrls = normalizeRelayUrlSet(['wss://brb.io']);
+		// #TODO: user should config outbox relays
+		const outboxRelayUrls = normalizeRelayUrlSet(["wss://purplepag.es"]);
 
-    ndk = new NDK({
-      explicitRelayUrls,
-      outboxRelayUrls,
-      blacklistRelayUrls,
-      enableOutboxModel: false,
-      autoConnectUserRelays: true,
-      autoFetchUserMutelist: true,
-      // clientName: 'Lume',
-      // clientNip89: '',
-    });
+		// #TODO: user should config blacklist relays
+		const blacklistRelayUrls = normalizeRelayUrlSet(["wss://brb.io"]);
 
-    await ndk.connect(10000);
+		ndk = new NDK({
+			cacheAdapter,
+			explicitRelayUrls,
+			outboxRelayUrls,
+			blacklistRelayUrls,
+			enableOutboxModel: false,
+			autoConnectUserRelays: true,
+			autoFetchUserMutelist: true,
+			// clientName: 'Lume',
+			// clientNip89: '',
+		});
 
-    // start fetcher instance
-    fetcher = NostrFetcher.withCustomPool(ndkAdapter(ndk));
-  })
-  .get('/', () => 'Use Lume to interact with nstation')
-  .get('/users/:id/profile', async ({ set, params: { id } }) => {
-    let pubkey: string = id;
+		await ndk.connect(10000);
 
-    if (pubkey.startsWith('npub1')) {
-      pubkey = nip19.decode(pubkey).data as string;
-    }
+		// start fetcher instance
+		fetcher = NostrFetcher.withCustomPool(ndkAdapter(ndk));
+	})
+	.get("/", () => "Use Lume to interact with nstation")
+	.get("/users/:id/profile", async ({ set, params: { id } }) => {
+		let pubkey: string = id;
 
-    if (pubkey.startsWith('nprofile1')) {
-      const decoded = nip19.decode(pubkey).data as ProfilePointer;
-      pubkey = decoded.pubkey;
-    }
+		if (pubkey.startsWith("npub1")) {
+			pubkey = nip19.decode(pubkey).data as string;
+		}
 
-    const user = ndk.getUser({ pubkey });
-    const profile = await user.fetchProfile();
+		if (pubkey.startsWith("nprofile1")) {
+			const decoded = nip19.decode(pubkey).data as ProfilePointer;
+			pubkey = decoded.pubkey;
+		}
 
-    if (!profile) {
-      set.status = 404;
-      throw new Error('Not found');
-    }
+		const user = ndk.getUser({ pubkey });
+		const profile = await user.fetchProfile();
 
-    return profile;
-  })
-  .get('/users/:id/contacts', async ({ set, params: { id } }) => {
-    let pubkey: string = id;
+		if (!profile) {
+			set.status = 404;
+			throw new Error("Not found");
+		}
 
-    if (pubkey.startsWith('npub1')) {
-      pubkey = nip19.decode(pubkey).data as string;
-    }
+		return profile;
+	})
+	.get("/users/:id/contacts", async ({ set, params: { id } }) => {
+		let pubkey: string = id;
 
-    if (pubkey.startsWith('nprofile1')) {
-      const decoded = nip19.decode(pubkey).data as ProfilePointer;
-      pubkey = decoded.pubkey;
-    }
+		if (pubkey.startsWith("npub1")) {
+			pubkey = nip19.decode(pubkey).data as string;
+		}
 
-    const user = ndk.getUser({ pubkey });
-    const contacts = [...(await user.follows())].map((user) => user.pubkey);
+		if (pubkey.startsWith("nprofile1")) {
+			const decoded = nip19.decode(pubkey).data as ProfilePointer;
+			pubkey = decoded.pubkey;
+		}
 
-    if (!contacts) {
-      set.status = 404;
-      throw new Error('Not found');
-    }
+		const user = ndk.getUser({ pubkey });
+		const contacts = [...(await user.follows())].map((user) => user.pubkey);
 
-    return contacts;
-  })
-  .get('/events/:id', async ({ set, params: { id } }) => {
-    const event = await ndk.fetchEvent(id, {
-      cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
-    });
+		if (!contacts) {
+			set.status = 404;
+			throw new Error("Not found");
+		}
 
-    if (!event) {
-      set.status = 404;
-      throw new Error('Not found');
-    }
+		return contacts;
+	})
+	.get("/events/:id", async ({ set, params: { id } }) => {
+		const event = await ndk.fetchEvent(id, {
+			cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
+		});
 
-    return event.rawEvent();
-  })
-  .get(
-    '/events/all',
-    async ({ set, body }) => {
-      const relays = [...ndk.pool.relays.values()].map((el) => el.url);
-      const filter: NDKFilter = JSON.parse(body.filter);
+		if (!event) {
+			set.status = 404;
+			throw new Error("Not found");
+		}
 
-      const rootIds = new Set();
-      const dedupQueue = new Set();
+		return event.rawEvent();
+	})
+	.get(
+		"/events/all",
+		async ({ set, body }) => {
+			const relays = [...ndk.pool.relays.values()].map((el) => el.url);
+			const filter: NDKFilter = JSON.parse(body.filter);
 
-      const events = await fetcher.fetchLatestEvents(
-        relays,
-        filter,
-        body.limit || FETCH_LIMIT,
-        {
-          asOf: body.pageParam === 0 ? undefined : body.pageParam,
-        }
-      );
+			const rootIds = new Set();
+			const dedupQueue = new Set();
 
-      if (!events) {
-        set.status = 404;
-        throw new Error('Not found');
-      }
+			const events = await fetcher.fetchLatestEvents(
+				relays,
+				filter,
+				body.limit || FETCH_LIMIT,
+				{
+					asOf: body.pageParam === 0 ? undefined : body.pageParam,
+				},
+			);
 
-      if (body.dedup) {
-        for (const event of events) {
-          const tags = event.tags.filter((el) => el[0] === 'e');
-          if (tags && tags.length > 0) {
-            const rootId = tags.filter((el) => el[3] === 'root')[1] ?? tags[0][1];
-            if (rootIds.has(rootId)) return dedupQueue.add(event.id);
-            rootIds.add(rootId);
-          }
-        }
+			if (!events) {
+				set.status = 404;
+				throw new Error("Not found");
+			}
 
-        return events
-          .filter((event) => !dedupQueue.has(event.id))
-          .sort((a, b) => b.created_at - a.created_at);
-      }
+			if (body.dedup) {
+				for (const event of events) {
+					const tags = event.tags.filter((el) => el[0] === "e");
+					if (tags && tags.length > 0) {
+						const rootId =
+							tags.filter((el) => el[3] === "root")[1] ?? tags[0][1];
+						if (rootIds.has(rootId)) return dedupQueue.add(event.id);
+						rootIds.add(rootId);
+					}
+				}
 
-      return events.sort((a, b) => b.created_at - a.created_at);
-    },
-    {
-      body: t.Object({
-        filter: t.String(),
-        limit: t.Optional(t.Number()),
-        pageParam: t.Number(),
-        dedup: t.Optional(t.Boolean()),
-      }),
-    }
-  )
-  .post('/events/:id/repost', async ({ set, params: { id } }) => {
-    if (!ndk.signer) {
-      set.status = 401;
-      throw new Error('NDK Signer is required');
-    }
+				return events
+					.filter((event) => !dedupQueue.has(event.id))
+					.sort((a, b) => b.created_at - a.created_at);
+			}
 
-    const event = await ndk.fetchEvent(id, {
-      cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
-    });
-    const repost = await event?.repost(true);
+			return events.sort((a, b) => b.created_at - a.created_at);
+		},
+		{
+			body: t.Object({
+				filter: t.String(),
+				limit: t.Optional(t.Number()),
+				pageParam: t.Number(),
+				dedup: t.Optional(t.Boolean()),
+			}),
+		},
+	)
+	.post("/events/:id/repost", async ({ set, params: { id } }) => {
+		if (!ndk.signer) {
+			set.status = 401;
+			throw new Error("NDK Signer is required");
+		}
 
-    return repost?.rawEvent();
-  })
-  .post(
-    '/events/:id/react',
-    async ({ set, params: { id }, body }) => {
-      if (!ndk.signer) {
-        set.status = 401;
-        throw new Error('NDK Signer is required');
-      }
+		const event = await ndk.fetchEvent(id, {
+			cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
+		});
+		const repost = await event?.repost(true);
 
-      const event = await ndk.fetchEvent(id, {
-        cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
-      });
-      const reaction = await event?.react(body.content ?? '👍');
+		return repost?.rawEvent();
+	})
+	.post(
+		"/events/:id/react",
+		async ({ set, params: { id }, body }) => {
+			if (!ndk.signer) {
+				set.status = 401;
+				throw new Error("NDK Signer is required");
+			}
 
-      return reaction?.rawEvent();
-    },
-    {
-      body: t.Object({
-        content: t.String(),
-      }),
-    }
-  )
-  .post(
-    '/events/:id/zap',
-    async ({ set, params: { id }, body }) => {
-      if (!ndk.signer) {
-        set.status = 401;
-        throw new Error('NDK Signer is required');
-      }
+			const event = await ndk.fetchEvent(id, {
+				cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
+			});
+			const reaction = await event?.react(body.content ?? "👍");
 
-      const event = await ndk.fetchEvent(id, {
-        cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
-      });
+			return reaction?.rawEvent();
+		},
+		{
+			body: t.Object({
+				content: t.String(),
+			}),
+		},
+	)
+	.post(
+		"/events/:id/zap",
+		async ({ set, params: { id }, body }) => {
+			if (!ndk.signer) {
+				set.status = 401;
+				throw new Error("NDK Signer is required");
+			}
 
-      const invoice = await event?.zap(body.amount, body.message);
+			const event = await ndk.fetchEvent(id, {
+				cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
+			});
 
-      return { invoice: invoice };
-    },
-    {
-      body: t.Object({
-        amount: t.Number(),
-        message: t.String(),
-      }),
-    }
-  )
-  .post(
-    '/events/filter',
-    async ({ set, body }) => {
-      const filter: NDKFilter = JSON.parse(body.filter);
-      const event = await ndk.fetchEvent(filter, {
-        cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
-      });
+			const invoice = await event?.zap(body.amount, body.message);
 
-      if (!event) {
-        set.status = 404;
-        throw new Error('Not found');
-      }
+			return { invoice: invoice };
+		},
+		{
+			body: t.Object({
+				amount: t.Number(),
+				message: t.String(),
+			}),
+		},
+	)
+	.post(
+		"/events/filter",
+		async ({ set, body }) => {
+			const filter: NDKFilter = JSON.parse(body.filter);
+			const event = await ndk.fetchEvent(filter, {
+				cacheUsage: NDKSubscriptionCacheUsage.CACHE_FIRST,
+			});
 
-      return event.rawEvent();
-    },
-    {
-      body: t.Object({
-        filter: t.String(),
-      }),
-    }
-  )
-  .post(
-    '/events/publish',
-    async ({ set, body }) => {
-      if (!ndk.signer) {
-        set.status = 401;
-        throw new Error('NDK Signer is required');
-      }
+			if (!event) {
+				set.status = 404;
+				throw new Error("Not found");
+			}
 
-      const event = new NDKEvent(ndk);
-      if (body.content) event.content = body.content;
-      event.kind = body.kind;
-      event.tags = body.tags;
+			return event.rawEvent();
+		},
+		{
+			body: t.Object({
+				filter: t.String(),
+			}),
+		},
+	)
+	.post(
+		"/events/publish",
+		async ({ set, body }) => {
+			if (!ndk.signer) {
+				set.status = 401;
+				throw new Error("NDK Signer is required");
+			}
 
-      if (body.rootReplyTo) {
-        const rootEvent = await ndk.fetchEvent(body.rootReplyTo);
-        if (rootEvent) event.tag(rootEvent, 'root');
-      }
+			const event = new NDKEvent(ndk);
+			if (body.content) event.content = body.content;
+			event.kind = body.kind;
+			event.tags = body.tags;
 
-      if (body.replyTo) {
-        const replyEvent = await ndk.fetchEvent(body.replyTo);
-        if (replyEvent) event.tag(replyEvent, 'reply');
-      }
+			if (body.rootReplyTo) {
+				const rootEvent = await ndk.fetchEvent(body.rootReplyTo);
+				if (rootEvent) event.tag(rootEvent, "root");
+			}
 
-      const publish = await event.publish();
+			if (body.replyTo) {
+				const replyEvent = await ndk.fetchEvent(body.replyTo);
+				if (replyEvent) event.tag(replyEvent, "reply");
+			}
 
-      if (!publish) {
-        set.status = 500;
-        throw new Error('Failed to publish event');
-      }
+			const publish = await event.publish();
 
-      // return total relays has been successfully publish that event
-      return publish.size;
-    },
-    {
-      body: t.Object({
-        content: t.String(),
-        kind: t.Number(),
-        tags: t.Array(t.Array(t.String())),
-        replyTo: t.String(),
-        rootReplyTo: t.String(),
-      }),
-    }
-  )
-  .listen(6090);
+			if (!publish) {
+				set.status = 500;
+				throw new Error("Failed to publish event");
+			}
+
+			// return total relays has been successfully publish that event
+			return publish.size;
+		},
+		{
+			body: t.Object({
+				content: t.String(),
+				kind: t.Number(),
+				tags: t.Array(t.Array(t.String())),
+				replyTo: t.String(),
+				rootReplyTo: t.String(),
+			}),
+		},
+	)
+	.listen(6090);
 
 export type App = typeof app;
---
+++ src/types.d.ts
@@ -0,0 +1,27 @@
+export interface NDKCacheUser {
+	pubkey: string;
+	profile: string | NDKUserProfile;
+	createdAt: number;
+}
+
+export interface NDKCacheUserProfile extends NDKUserProfile {
+	pubkey: string;
+}
+
+export interface NDKCacheEvent {
+	id: string;
+	pubkey: string;
+	content: string;
+	kind: number;
+	createdAt: number;
+	relay: string;
+	event: string;
+}
+
+export interface NDKCacheEventTag {
+	id: string;
+	eventId: string;
+	tag: string;
+	value: string;
+	tagValue: string;
+}
